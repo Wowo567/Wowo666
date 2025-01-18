899dae2fa4aab1f9b648748957de2ec93b283d3a
Tree <387786238@qq.com>
2025-01-18 16:31:39 +0800

    漫画prefab脚本

--- Global Game Jam 2025/Assets/Scripts/Comic/ComicItem.cs
+++ Global Game Jam 2025/Assets/Scripts/Comic/ComicItem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UI;
 using UnityEngine;
 
 public enum BubbleType
@@ -11,10 +12,33 @@ public enum BubbleType
     
 public class ComicItem : MonoBehaviour
 {
+    public int id;
+    private ComicData _comicData;
     private Dictionary<int, ComicItem> _nextComicItems = new Dictionary<int, ComicItem>();
+
+    private ChatBubblePoint _point;
+    private void Awake()
+    {
+       Init();
+    }
+
+    private void Init()
+    {
+        //初始位置
+        _comicData = DatasManager.Instance.comicItemDatas.DatasDic[id];
+        transform.position = _comicData.position;
+        
+        _point = GetComponentInChildren<ChatBubblePoint>();
+        _point.OnBubble+= OnBubble;
+        _point.OnBubbleRemove+= OnBubbleRemove;
+    }
+
+    private void OnBubble(int type)
+    {
+        
+    }
     
-    // Start is called before the first frame update
-    void Start()
+    private void OnBubbleRemove()
     {
         
     }
