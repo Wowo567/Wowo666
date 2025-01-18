fc2da94362b64d5153486ec8e853c9eddd8eae7c
Tree <387786238@qq.com>
2025-01-18 20:26:02 +0800

    结构更改，渐隐

--- Global Game Jam 2025/Assets/Scripts/Comic/ComicItem.cs
+++ Global Game Jam 2025/Assets/Scripts/Comic/ComicItem.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Datas;
+using DG.Tweening;
 using UI;
 using UnityEngine;
 
@@ -19,9 +20,10 @@ namespace Comic
         private ComicData _comicData;
         private ComicItem _nextComic;
         private Dictionary<BubbleType, ComicItem> _nextComicItems = new Dictionary<BubbleType, ComicItem>();
-
-        private SpriteRenderer _face;
+        
         private ChatBubblePoint _point;
+        private SpriteRenderer[] _grey, _color;
+        private float fadeTime = 0.3f;
         
         private void Start()
         {
@@ -33,24 +35,42 @@ namespace Comic
             //初始位置
             _comicData = DatasManager.Instance.comicItemDatas.DatasDic[id];
             transform.position = _comicData.position;
-            //_face = transform.Find("Face").GetComponent<SpriteRenderer>();
+            _grey = transform.Find("Grey").GetComponentsInChildren<SpriteRenderer>();
+            _color = transform.Find("Color").GetComponentsInChildren<SpriteRenderer>();
             
             //更改相机
             CameraManager.Instance.ChangeCamera(this);
+            //显示线稿
+            GreyShow(true);
         
             _point = GetComponentInChildren<ChatBubblePoint>();
             _point.Init(_comicData.nextComics.Keys.ToList());
             _point.OnBubble+= OnBubble;
             _point.OnBubbleRemove+= OnBubbleRemove;
         }
+
+        private void GreyShow(bool isShow)
+        {
+            foreach (var item in _grey)
+            {
+                item.DOFade(isShow ? 1 : 0, fadeTime);
+            }
+        }
+        
+        private void ColorShow(bool isShow)
+        {
+            foreach (var item in _color )
+            {
+                item.DOFade(isShow ? 1 : 0, fadeTime);
+            }
+        }
+
         
         private void OnBubble(BubbleType type)
         {
-            BubbleData bubbleData = _comicData.nextComics[type];
+            ColorShow(true);
+            int next = _comicData.nextComics[type];
             
-            //更改表情
-            //Sprite faceSprite = DatasManager.Instance.faceDatas.DatasDic[bubbleData.faceId].sprite;
-            // _face.sprite = faceSprite;
             //实例下一个漫画
             // _nextComic = bubbleData.nextId;
             
