8f777145d861742727ba4d7636ce703dea76a21d
Wowo567 <1446989872@qq.com>
2025-01-19 17:21:16 +0800

    jieju

---
+++ Global Game Jam 2025/Assets/Scripts/EndTag.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTag : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(nameof(WaitToOver));
+    }
+
+    IEnumerator WaitToOver()
+    {
+        yield return new WaitForSeconds(5);
+        CameraManager.Instance.EndAnim();
+    }
+}
